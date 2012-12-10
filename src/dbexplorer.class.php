f44d1b61e5648bfbd23a484e9a203d565b1c7efe
bpaulin <brunopaulin@bpaulin.net>
2012-12-10 22:43:51 +0100

    correction cs

--- src/dbexplorer.class.php
+++ src/dbexplorer.class.php
@@ -4,52 +4,57 @@
 */
 class DbExplorer
 {
-	protected $host;
-	protected $user;
-	protected $pass;
-	protected $base;
-	protected $table;
+    protected $host;
+    protected $user;
+    protected $pass;
+    protected $base;
+    protected $table;
 
-	protected $db;
+    protected $db;
 
-	public function connect($host, $user, $pass, $base, $table)
-	{
-		$this->host = $host;
-		$this->user = $user;
-		$this->pass = $pass;
-		$this->base = $base;
-		$this->table = $table;
-		$this->db = new mysqli($host, $user, $pass, $base);
-		return $this->db;
-	}
+    public function connect($host, $user, $pass, $base, $table)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->base = $base;
+        $this->table = $table;
+        $this->db = new mysqli($host, $user, $pass, $base);
 
-	public function describeTable(){
-		$retour = array();
-		if ($result = $this->db->query("DESCRIBE `".$this->base . "`.`" . $this->table . "`")) {
-			while ($row = $result->fetch_assoc()) {
-				$retour[] = $row;
-			}
-		}
-		return $retour;
-	}
+        return $this->db;
+    }
 
-	public function select($field, $strict, $value){
-		$query = "SELECT * 
-				  FROM `".$this->base . "`.`" . $this->table . "`";
-		if ($field) {
-			if ($strict) {
-				$compare = "='{$this->db->escape_string($value)}'";
-			} else {
-				$compare = "LIKE '%{$this->db->escape_string($value)}%'";
-			}
-			$query .= "WHERE {$this->db->escape_string($field)} $compare";	
-		}
-		$retour = array();
-		if ($result = $this->db->query($query)) {
-			while ($row = $result->fetch_assoc()) {
-				$retour[] = $row;
-			}
-		}
-		return $retour;
-	}
+    public function describeTable()
+    {
+        $retour = array();
+        if ($result = $this->db->query("DESCRIBE `".$this->base . "`.`" . $this->table . "`")) {
+            while ($row = $result->fetch_assoc()) {
+                $retour[] = $row;
+            }
+        }
+
+        return $retour;
+    }
+
+    public function select($field, $strict, $value)
+    {
+        $query = "SELECT *
+                  FROM `".$this->base . "`.`" . $this->table . "`";
+        if ($field) {
+            if ($strict) {
+                $compare = "='{$this->db->escape_string($value)}'";
+            } else {
+                $compare = "LIKE '%{$this->db->escape_string($value)}%'";
+            }
+            $query .= "WHERE {$this->db->escape_string($field)} $compare";
+        }
+        $retour = array();
+        if ($result = $this->db->query($query)) {
+            while ($row = $result->fetch_assoc()) {
+                $retour[] = $row;
+            }
+        }
+
+        return $retour;
+    }
 }
